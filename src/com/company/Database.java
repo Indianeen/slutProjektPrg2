8ff1d8a20d146446e69e83c497ca351c5f4d2878
Indianeen <axel.lundberg@elev.ga.ntig.se>
2023-05-11 13:44:44 +0200

    Databas borde fungera nu/ snart (ej testat)

--- src/com/company/Database.java
+++ src/com/company/Database.java
@@ -11,6 +11,43 @@ public class Database {
         JPasswordField passwordField = new JPasswordField();
         JOptionPane.showConfirmDialog(null, passwordField, "password?", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
         String password = new String(passwordField.getPassword());
-        
+
+        try {
+
+            conn = DriverManager.getConnection();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        };
+
+
+        try {
+            Statement stmt = conn.createStatement();
+            String SQLQuery = "SELECT * FROM ---";
+            ResultSet rs = stmt.executeQuery(SQLQuery);
+
+            ResultSetMetaData metaData = rs.getMetaData();
+
+            int numCols = metaData.getColumnCount();
+            for (int i = 1 ; i <= numCols ; i++) {
+                System.out.println(metaData.getColumnClassName(i));
+            }
+
+            while (rs.next()) {
+                String output = "";
+                output += rs.getInt("id"); // + "," +
+                //        rs.getString("title") + "," +
+                //        rs.getString("author") + "," +
+                //        rs.getDouble("price") + "," +
+                //        rs.getInt("quantity");
+                System.out.println(output);
+            }
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/com/company/DatabaseLoginData.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class DatabaseLoginData {
+    static String user = "";
+    static String password = "";
+    static String port = "";
+    static String DBname = "";
+    static String DBURL = "";
+}
